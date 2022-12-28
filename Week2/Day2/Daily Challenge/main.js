84bd25e8a1522fcbed83a4a382759dcab1281b93
Aymar <yannyao4066@gmail.com>
2022-12-28 15:08:27 +0000

    Challenge commit

---
+++ Week2/Day2/Daily Challenge/main.js
@@ -0,0 +1,10 @@
+const sentence = "The movie is not blalalalalasdfghjkhbcvjkhgcfhkjhdygh bad, I like it";
+const wordNot = sentence.indexOf("not");
+const wordBad = sentence.indexOf("bad");
+
+if (wordNot !== -1 && wordBad !== -1 && wordNot < wordBad) {
+  const newSentence = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+  console.log(newSentence);
+} else {
+  console.log(sentence);
+}
